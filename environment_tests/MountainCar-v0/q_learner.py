d111bd92db612b2063c1781839be2afa76eaa9a7
Ricardo C <ricardo@suggestic.com>
2018-10-31 10:27:21 -0600

    add learn() Q_learner method

--- environment_tests/MountainCar-v0/q_learner.py
+++ environment_tests/MountainCar-v0/q_learner.py
@@ -16,13 +16,13 @@ class Q_learner(object):
         self.obs_bin = NUM_DISCRETE_BINS
         self.bin_width = (self.obs_high - self.obs_low) / self.obs_bins
         self.action_shape = env.action_space.n
-        self.Q = np.zeros((self.obs_bins + 1, self.obs_bins + 1, 
+        self.Q = np.zeros((self.obs_bins + 1, self.obs_bins + 1,
                            self.action_shape))
         self.alpha = ALPHA
         self.gamma = GAMMA
         self.epsilon = 1.0
 
-    def discretize(self,obs):
+    def discretize(self, obs):
         return tuple(((obs - self.obs_low) / self.bin_width).astype(int))
 
     def get_action(self, obs):
@@ -33,4 +33,12 @@ class Q_learner(object):
             return np.argmax(self.Q[discretized_obs])
         else:
             return np.random.choice([a for a in range(self.action_shape)])
-            
+
+    def learn(self, obs, action, reward, next_obs):
+        discretized_obs = self.discretize(obs)
+        discretized_next_obs = self.discretize(next_obs)
+        td_target = reward + self.gamma * np.max(
+            self.Q[discretized_next_obs]
+        )
+        td_error = td_target - self.Q[discretized_obs][action]
+        self.Q[discretized_obs][action] += self.alpha * td_error
