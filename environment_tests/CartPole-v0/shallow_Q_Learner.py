7e29b23ee4bd8ce10b1d0b2af1080fd84cb281bc
Ricardo C <ricardo@suggestic.com>
2018-11-07 22:19:47 -0600

    add __init__() to Shallow_Q_Learner class

--- environment_tests/CartPole-v0/shallow_Q_Learner.py
+++ environment_tests/CartPole-v0/shallow_Q_Learner.py
@@ -7,3 +7,27 @@ from torch.autograd import Variable
 
 from function_approxumator.perceptron import SLP
 from utils.decay_schedule import LinearDecaySchedule
+
+env = gym.make('CartPole-v0')
+MAX_NUM_EPISODES = 10000
+MAX_STEPS_PER_EPISODE = 300
+
+class Shallow_Q_Learner(object):
+    def __init__(self,
+                 state_shape,
+                 action_shape,
+                 learning_rate=0.005,
+                 gamma=0.98):
+        self.state_shape = state_shape
+        self.action_shape = action_shape
+        self.learning_rate = learning_rate
+        self.Q = SLP(state_shape, action_shape)
+        self.Q_optimizer = torch.optim.Adam(self.Q.parameters(), lr=1e-3)
+        self.policy = self.epsilon_greedy_Q
+        self.epsilon_max = 1.0
+        self.epsilon_min = 0.05
+        self.epsilon_decay = LinearDecaySchedule(initial_value=self.epsilon_max,
+                                                 final_value=self.epsilon_min,
+                                                 max_steps=0.5*MAX_NUM_EPISODES * MAX_STEPS_PER_EPISODE)
+        self.step_num = 0
+        
