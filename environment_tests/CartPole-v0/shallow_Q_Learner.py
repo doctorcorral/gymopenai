c7778d90120df0c4742672f597613b76b582d654
Ricardo C <ricardo@suggestic.com>
2018-11-08 22:17:20 -0600

    fix function_approximator import name

--- environment_tests/CartPole-v0/shallow_Q_Learner.py
+++ environment_tests/CartPole-v0/shallow_Q_Learner.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 
-from function_approxumator.perceptron import SLP
+from function_approximator.perceptron import SLP
 from utils.decay_schedule import LinearDecaySchedule
 
 env = gym.make('CartPole-v0')
